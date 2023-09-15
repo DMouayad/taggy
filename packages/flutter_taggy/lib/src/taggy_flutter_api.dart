425234b3da29d96edbf2cd930c1ef8d24509f40b
DMouayad <muayad.perun@outlook,com>
2023-09-15 16:40:01 +0300

    chore(flutter_taggy): document public APIs

--- packages/flutter_taggy/lib/src/taggy_flutter_api.dart
+++ packages/flutter_taggy/lib/src/taggy_flutter_api.dart
@@ -4,6 +4,11 @@ import 'ffi/stub.dart'
     if (dart.library.html) 'ffi/web.dart';
 
 extension FlutterTaggyExtension on TaggyInterface {
+  /// Initializes the library in a Flutter app.
+  /// **Must** be called before using this package.
+  ///
+  /// Note: an internet connection is required, the first time only,
+  /// to download the required binaries from the internet.
   void initialize() {
     initializeFrom(createLibraryImpl());
   }
